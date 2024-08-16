5f485b7aec6ccbc46b884de01425949ec37d29d5
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-08-16 16:30:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,||"v3YmwSgJ#53gh-EYj0AqpVzBcSheEMt7dv8dsTGz5tpEHQIEy8P4"
+SESSION_ID: process.env.SESSION_ID||"v3YmwSgJ#53gh-EYj0AqpVzBcSheEMt7dv8dsTGz5tpEHQIEy8P4",
 ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "", 
 };
