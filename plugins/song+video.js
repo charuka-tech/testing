a597885878a44cfda8e9086e8212c87edf6bb491
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-08-17 09:12:35 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,28 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yts-search')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try}
+
+
+
+
+
+} catch (e){
+console.log(e)
+reply (`${e}`)
+}
+    
+
+
+
+
 
