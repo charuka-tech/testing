cdb5cd4101c55fd4dc6658ca988cf922d0462265
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-08-17 13:13:18 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,4 +1,3 @@
-const config = require('../config')
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yts-search')
@@ -11,6 +10,43 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try}
+
+    
+if(!q) return reply ("please give me url or name")
+
+const search= await yts (q)
+const data= search.videos[0]
+const url= data.url
+
+
+let desc= `
+
+
+`🎵DARK CYBER SONG DOWNLOADER🎵`
+
+📍title: ${data.title}
+🧬description: ${}
+🔮time: ${data.timestamp}
+📌ago: ${data.ago}
+📶views: $ {dta.views}
+
+> 🎵MADE BY DARK CYBER🎵
+
+await conn.sendMessage  (from,{ image:{url:data.thumbnail},caption: desc,quoted:mak);
+
+
+// download audio
+
+let down =await.fg.uta(url)
+let download =down.dl_url
+
+
+// send audio message 
+
+
+await conn sendMessage  (from,{audio:{url:download url},mimetype:"audio/mpeg"},{quoted:mek})
+
+    
 
 
 
