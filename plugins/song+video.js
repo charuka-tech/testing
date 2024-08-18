e35b627d2eeb623a3b2fea1898920673e1d86da4
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-08-18 15:41:19 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -4,25 +4,25 @@ const yts = require('yts-search')
 
 cmd({
     pattern: "song",
-    desc: "download song",
+    desc: "download songs",
     category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try}
+try{
 
     
 if(!q) return reply ("please give me url or name")
 
-const search= await yts (q)
-const data= search.videos[0]
-const url= data.url
+const search = await yts (q)
+const data = search.videos[0];
+const url = data.url
 
 
 let desc= `
 
 
-> 🎵DARK CYBER SONG DOWNLOADER🎵
+🎵DARK CYBER SONG DOWNLOADER🎵
 
 📍title: ${data.title}
 🧬description: ${data.description}
@@ -30,102 +30,23 @@ let desc= `
 📌ago: ${data.ago}
 📶views: ${data.views}
 
-> 🎵MADE BY DARK CYBER🎵 `
+🎵MADE BY DARK CYBER🎵
 
-await conn.sendMessage  (from,{ image:{url:data.thumbnail},caption: desc,quoted:mak);
+   `
+
+await conn.sendMessage(from,{ image:{url:data.thumbnail},caption: desc},{quoted:mak});
 
 
 // download audio
-
-let down =await.fg.uta(url)
-let download =download url.dl_url
-
-
-// send audio + decumet message 
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
 
 
-await conn sendMessage  (from,{audio:{url:download url},mimetype:"audio/mpeg"},{quoted:mek})
-await conn sendMessage  (from,{document:{url:download url},mimetype:"audio/mpeg",fileName:data.title + "mp3",caption:"MADE BY DARK CYBER MD"},{quoted:mek})
+// send audio message
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek});
 
-
-    
-
-
-
-
-
-} catch (e){
+}catch(e){
 console.log(e)
-reply (`${e}`)
-}
-})
-    
-
-
-
-
-
-//=============================VIDEO-DL==========================
-
-
-
-
-
-cmd({
-    pattern: "video",
-    desc: "download video",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try}
-
-    
-if(!q) return reply ("please give me url or name")
-
-const search= await yts (q)
-const data= search.videos[0]
-const url= data.url
-
-
-let desc= `
-
-
-> 📽️DARK CYBER VIDEO DOWNLOADER📽️
-
-📍title: ${data.title}
-🧬description: ${data.description}
-🔮time: ${data.timestamp}
-📌ago: ${data.ago}
-📶views: ${data.views}
-
-> 🎵MADE BY DARK CYBER🎵 `
-
-await conn.sendMessage  (from,{ image:{url:data.thumbnail},caption: desc,quoted:mak);
-
-
-// download video
-
-let down =await.fg.uta(url)
-let download =download url.dl_url
-
-
-// send video + document message 
-
-
-await conn sendMessage  (from,{video:{url:download url},mimetype:"video/mp4"},{quoted:mek})
-await conn sendMessage  (from,{document:{url:download url},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"MADE BY DARK CYBER MD"},{quoted:mek})
-
-
-
-    
-
-
-
-
-
-} catch (e){
-console.log(e)
-reply (`${e}`)
+reply(`${e}`)
 }
 })
