4aaab9de32327a1bc250f6e5544de8e38d27d4e7
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-08-19 19:47:38 +0530

    Add files via upload

---
+++ plugins/song+video.js
@@ -0,0 +1,98 @@
+const {cmd , commands} = require('../command')
+const fg = require(`api-dylux`)
+const yts = require(`yt-search`)
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url of name")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+
+let desc = `
+
+
+🎶`DARK CYBER SONG DOWNLOADER`🎶
+
+📍title: ${data.title}
+🧬description: ${data.description}
+🔮time: ${data.timestamp}
+📌ago: ${data.ago}
+📶views: ${data.views}
+
+🎧`MADE BY DARK CYBER`🎧
+
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio + document message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY DARK CYBER"},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+//==========video-dl=======
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url of name")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+
+let desc = `
+
+
+📽️`DARK CYBER VIDEO DOWNLOADER`📽
+
+📍title: ${data.title}
+🧬description: ${data.description}
+🔮time: ${data.timestamp}
+📌ago: ${data.ago}
+📶views: ${data.views}
+
+📽️`MADE BY DARK CYBER`📽️
+
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video + document message
+
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"MADE BY DARK CYBER"},{quoted:mek})
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
