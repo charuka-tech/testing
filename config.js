b869f24f0ada87d7c7f6191e662bce83f465836c
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-04 10:01:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,3 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID||"Wr4FEJJL#9Mec3asX3Z7Vw3FUGYcHadaBAC_rsER2fDTYqDYmL0o",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "•ᴀʟɪᴠᴇ ɴᴏᴡ•
-              
-> 📅 ᴜᴘᴛɪᴍᴇ: ${uptime}
-> 📡 ᴘʟᴀᴛғᴏʀᴍ: *${os.platform()}*
-> 🛡 ᴍᴏᴅᴇ: *${mode}*
-> 💫 ᴘʀᴇғɪx: [ . ]   " ,
-};
