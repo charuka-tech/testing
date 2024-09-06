2bb188b517848551dbeffed7b42a43714a55e9e0
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-06 09:34:50 +0530

    Update news (1).js

--- plugins/news (1).js
+++ plugins/news (1).js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*
+  *© ᴍᴀᴅᴇ ʙʏ ᴍɪᴢᴜᴋɪ ᴍᴅッ*
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
