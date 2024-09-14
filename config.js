0ef33d28b803d696b4555224472712aa1af18ebb
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-14 18:55:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID||"0msmTJrD#teQpWm4dYfiWwwRve1eLSYahclmtK79OgFGgBJ4JAcA",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json"
+OMDB_API_KEY: process.env.OMDB_API_KEY || "https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json",
 };
