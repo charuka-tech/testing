7963665f0c93077672932365b99ea3f8a68301d4
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-15 10:35:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID||"0msmTJrD#teQpWm4dYfiWwwRve1eLSYahclmtK79OgF
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json",
+OWNER_NUMBER: proaess.env.OWNER_NUMBER || "94775228949"
 };
