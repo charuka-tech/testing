5ee01db606f4e5ffdbe2961b4f01879ff60cdb8a
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-15 10:58:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID||"0msmTJrD#teQpWm4dYfiWwwRve1eLSYahclmtK79OgFGgBJ4JAcA",
+SESSION_ID: process.env.SESSION_ID||"nJk02IIR#XCZS46dML8uomQU8KG5y6paFdAcGq0ZQvQZZwYhkzeA",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json",
