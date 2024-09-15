ef785c7e1f199d7f81e0dfdf111fdc14724cec1c
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-15 10:32:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
 
-const ownerNumber = ['94775228949']
+const ownerNumber = ['{config.OWNER_NUMBER}']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
