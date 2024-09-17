976d2ad2cdb24e81cb24d5849633b86a831a570f
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-09-17 10:09:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID||"nJk02IIR#XCZS46dML8uomQU8KG5y6paFdAcGq0ZQvQZZwYhkzeA",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json",
+//--------------------------------//--------------------------------//
+OMDB_API_KEY: process.env.OMDB_API_KEY="76cb7f39",
 };
