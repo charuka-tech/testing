788ed82364507d31138261201918c9c74bb3e0a0
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-09-23 15:39:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID||"qAl31ZLQ#pK8Ty94-OrZEVzyVgfJkFK1kREjRQCqPkB0izbMh_G4",
+SESSION_ID: process.env.SESSION_ID||"EjFC1aCT#qOCS_1jGnN92cmO_zu4RPS3BhsxV7ahBoHTvrf6p4_8",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 //--------------------------------//--------------------------------//
