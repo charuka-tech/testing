b47c633600776e73d067631e35f4021cb4ae83cd
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-09-26 16:11:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID||"Qmsj3Lqb#oryKlQ1KterhDc4MR8qjJd7BUga4RpTka8u7f6eAg5k",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 //--------------------------------//--------------------------------//
 OMDB_API_KEY: process.env.OMDB_API_KEY="76cb7f39",
 };
