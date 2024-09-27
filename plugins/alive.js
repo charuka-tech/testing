1add0170b4429293f7d32ddad03a4c829908fd01
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-09-27 15:14:28 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -20,7 +20,7 @@ let cyber = `👋 HELLO ${pushname} I'm alive now
 *🚀Version:* ${require("../package.json").version}
 *⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *🕒Runtime:* ${runtime(process.uptime())}
-*📍Platform:* ${hostname}
+*📍Platform:* ${os.hostname()}
 🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
                     
 *🌻Have A Nice Day..*🌻
