0760722047e5c1ad9049170991d745fd461b37ea
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-10-07 15:50:20 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
 const{runtime} = require('../lib/functions')
-const { generateWAMessageFromContent, proto } = pkg;
+const { generateWAMessageFromContent, proto }
 
 
 
