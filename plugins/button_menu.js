cb905b9e809276517f5bb3ec99dc154636427b10
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-10-19 19:43:19 +0530

    Update button_menu.js

--- plugins/button_menu.js
+++ plugins/button_menu.js
@@ -17,7 +17,7 @@ cmd({
   type: "user", // Command type
 
 }, async (conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try => {
+try{
 
     let { prefix } = message;
 
